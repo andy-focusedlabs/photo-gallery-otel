0baaffc121f7cf80f90f1963ddfa56420253267b
Andy Ennamorato <andy.ennamorato+github@focusedlabs.io>
2024-11-01 16:36:24 -0600

    Want to get a basic example working
    * Given a valid Honeycomb ingest key, data should show up pretty much immediately.

---
+++ README.md
@@ -0,0 +1,17 @@
+## What is it?
+
+An example of using the Honeycomb SDK in an Ionic project.
+
+Based on the [Ionic react starter](https://ionicframework.com/docs/react/your-first-app) and the [Honeycomb SDK docs](https://docs.honeycomb.io/send-data/javascript-browser/honeycomb-distribution/).
+
+## Running It
+
+Get a Honeycomb ingest key and edit `main.tsx`:
+
+```tsx
+    apiKey: 'hcaik_01...', // Replace with your Honeycomb Ingest API Key.
+```
+
+Then, run `ionic serve`.
+Reload the page a few times and you should see traces in Honeycomb.
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import { HoneycombWebSDK } from '@honeycombio/opentelemetry-web';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+
+const configDefaults = {
+    ignoreNetworkEvents: true,
+    // propagateTraceHeaderCorsUrls: [
+    // /.+/g, // Regex to match your backend URLs. Update to the domains you wish to include.
+    // ]
+}
+
+const sdk = new HoneycombWebSDK({
+    // endpoint: "https://api.eu1.honeycomb.io/v1/traces", // Send to EU instance of Honeycomb. Defaults to sending to US instance.
+    debug: true, // Set to false for production environment.
+    apiKey: 'hcaik_01...', // Replace with your Honeycomb Ingest API Key.
+    serviceName: 'ionic-poc', // Replace with your application name. Honeycomb uses this string to find your dataset when we receive your data. When no matching dataset exists, we create a new one with this name if your API Key has the appropriate permissions.
+    instrumentations: [getWebAutoInstrumentations({
+        // Loads custom configuration for xml-http-request instrumentation.
+        '@opentelemetry/instrumentation-xml-http-request': configDefaults,
+        '@opentelemetry/instrumentation-fetch': configDefaults,
+        '@opentelemetry/instrumentation-document-load': configDefaults,
+    })],
+});
+sdk.start();
+
+// Call the element loader before the render call
+defineCustomElements(window);
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
